16f455500e47c829a17e2d33e9f50b6b0e0bc3ed
Bob <bob@liveramp.com>
2017-03-15 12:46:01 -0400

    Added cut_staging function

---
+++ config/fish/functions/cut_staging.fish
@@ -0,0 +1,17 @@
+function cut_staging
+  set -l week_number (printf "%02d" (math (date "%W") + 1))
+  set -l branch_name "staging/$week_number"
+
+  git checkout master
+    and git pull origin master
+    and git checkout -b $branch_name
+    or return
+
+  read -l -p "Push $branch_name? [y/N] " push_branch
+  switch $push_branch
+    case Y y Yes yes YES
+      git push origin $branch_name -u
+    case '*'
+      echo Did not push $branch_name
+  end
+end
