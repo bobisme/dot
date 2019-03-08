afffa672cc89c6fadb8e993606e7609fea2ba44c
Bob <bob@liveramp.com>
2019-03-07 21:37:43 -0500

    added k and s functions for kubectl

---
+++ config/fish/functions/k.fish
@@ -0,0 +1,29 @@
+function k
+  set -lx last_kubectl (history --search --prefix --max=1 kubectl | head -n1)
+
+  set -lx context (
+    string match --regex '[-]{2}context[= ][-\w]+' "$last_kubectl" \
+      | string replace ' ' '=' \
+      | string split -m1 '=' \
+      | tail -n1
+  )
+
+  set -lx namespace_arg (string match --regex '[-]{2}namespace[= ][-\w]+' "$last_kubectl")
+    or set -lx namespace_arg (string match --regex '[-]{1}n[= ][-\w]+' "$last_kubectl")
+  set -lx namespace (
+      string replace ' ' '=' -- "$namespace_arg" \
+      | string split -m1 '=' \
+      | tail -n1
+  )
+
+  commandline "kubectl"
+  commandline -it -- " --context="
+  commandline -it -- (string escape -- "$context")
+  if test -n "$namespace"
+    commandline -it -- " --namespace="
+    commandline -it -- (string escape -- "$namespace")
+  end
+  commandline -it -- " $argv"
+  commandline -f repaint
+end
+
