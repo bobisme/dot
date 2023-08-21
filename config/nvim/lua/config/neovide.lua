93a07c1ce104954ded83969ff2707e48f8b37ca2
Bob <bob@standardinput.co>
2023-08-21 13:58:30 -0400

    nvim: add neovide config

---
+++ config/nvim/lua/config/neovide.lua
@@ -0,0 +1,10 @@
+if vim.g.neovide then
+  vim.o.guifont = "FantasqueSansM Nerd Font"
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_cursor_animation_length = 0.05
+  vim.g.neovide_cursor_trail_size = 0.5
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_particle_density = 10.0
+  vim.g.neovide_cursor_vfx_opacity = 300.0
+end
