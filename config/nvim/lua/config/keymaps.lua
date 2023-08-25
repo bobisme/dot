6d062350ea25625d125cedb2ffa860ec744a4836
Bob <bob@standardinput.co>
2023-08-25 14:26:01 -0400

    nvim: keymaps to append semis and commas

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,4 @@
 vim.keymap.set({ "n", "v" }, "<c-k>", "10k", { desc = "Move up 10 lines.", remap = true })
 vim.keymap.set({ "n", "v" }, "<c-j>", "10j", { desc = "Move down 10 lines.", remap = true })
+vim.keymap.set({ "n" }, "<c-;>", "m':norm A;<cr>`'", { desc = "append semicolon;", remap = true, silent = true })
+vim.keymap.set({ "n" }, "<c-,>", "m':norm A,<cr>`'", { desc = "append comma,", remap = true, silent = true })
