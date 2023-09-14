7e9d11b3c3c1b9d585e7c9a2a238d42afef3d150
Bob <bob@standardinput.co>
2023-09-13 23:59:18 -0400

    nvim: L for open diagnostic floating window

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,4 @@ vim.keymap.set({ "n", "v" }, "<c-k>", "10k", { desc = "Move up 10 lines.", remap
 vim.keymap.set({ "n", "v" }, "<c-j>", "10j", { desc = "Move down 10 lines.", remap = true })
 vim.keymap.set({ "n" }, "<c-;>", "m':norm A;<cr>`'", { desc = "append semicolon;", remap = true, silent = true })
 vim.keymap.set({ "n" }, "<c-,>", "m':norm A,<cr>`'", { desc = "append comma,", remap = true, silent = true })
+vim.keymap.set({ "n" }, "L", vim.diagnostic.open_float, { desc = "append comma,", remap = true, silent = true })
